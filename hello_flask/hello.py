dd07822633bf764daa8b85f1766f1ea2c5b05d9b
HannahMulligan-hm <hannahx08@comcast.net>
2022-03-04 19:21:35 -0500

    more flask

--- hello_flask/hello.py
+++ hello_flask/hello.py
@@ -3,7 +3,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return 'Hello World!'
+	return redner_template("index.html")
+
+if __name__=="__main__":
+	app.run(debug=True)
+# @app.route('/')
+# def hello_world():
+# 	return 'Hello World!'
 
 @app.route('/success')
 def success():
