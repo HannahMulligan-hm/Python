4993e7ed1015fd6be32bc2c67ecbf4c705f407d5
HannahMulligan-hm <hannahx08@comcast.net>
2022-03-04 19:32:41 -0500

    simple template engine

--- hello_flask/hello.py
+++ hello_flask/hello.py
@@ -1,27 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-	return redner_template("index.html")
-
+def index():
+	return render_template("index.html", phrase="hello", times=5)
 if __name__=="__main__":
 	app.run(debug=True)
 # @app.route('/')
 # def hello_world():
 # 	return 'Hello World!'
 
-@app.route('/success')
-def success():
-	return "success"
+# @app.route('/success')
+# def success():
+# 	return "success"
 
-@app.route('/hello/<name>')
-def hello(name):
-	print(name)
-	return "Hello, " + name
+# @app.route('/hello/<name>')
+# def hello(name):
+# 	print(name)
+# 	return "Hello, " + name
 
-@app.route('/users/<username>/<id>')
-def show_user_profile(username, id):
-	print(username)
-	print(id)
-	return "username: "+ username + ", id: " + id
+# @app.route('/users/<username>/<id>')
+# def show_user_profile(username, id):
+# 	print(username)
+# 	print(id)
+	# return "username: "+ username + ", id: " + id
